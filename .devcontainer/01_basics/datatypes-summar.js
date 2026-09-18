df22d96ad1b09fa96904900efccbf620171425bc
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-18 11:31:45 +0000

    Data types Summary

---
+++ .devcontainer/01_basics/datatypes-summar.js
@@ -0,0 +1,20 @@
+// Primitive data types 
+
+// 7 types : string ,number ,boolen ,null ,undefined,Symbol,BinInt 
+
+let score =100;
+
+
+let scoreValue = 100.01 
+
+let logIn =false;
+let temp = null;
+let userEmail;
+
+
+
+
+
+// Reference (Non  Primitive)
+
+// Array, Objects, Functions 
