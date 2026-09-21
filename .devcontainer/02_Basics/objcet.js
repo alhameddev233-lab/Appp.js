b196693eb54707c5e73b557b2b205239cf89eed0
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-21 07:54:53 +0000

    Object.js-1

---
+++ .devcontainer/02_Basics/objcet.js
@@ -0,0 +1,39 @@
+
+
+        // Single-Ton
+
+        // Object-Literals 
+
+        const mySym = Symbol("Key1")
+        let jsUser = {
+            name: "Ahmed",
+            "fullname": "Ahmed AL-Hamed",
+            [mySym]: "Keysym-1",
+            age: 21,
+            location: "Pune",
+            email: "alhameda224@gmail.com",
+            isLogin: false,
+        }
+        // console.log(jsUser.name);
+        // console.log(jsUser["location"]);
+        // console.log(jsUser["fullname"]);
+        // console.log(jsUser[mySym]);
+
+        jsUser.email = "AlhmedDev@gmail.com"
+        // Object.freeze(jsUser)
+
+        jsUser.email = "AlhmedDev@google.com"
+        // console.log(jsUser);
+
+        jsUser.greeting = function () {
+            console.log("Hello js user");
+
+        }
+        jsUser.greetingTow = function () {
+            console.log(`Hello js user, ${this.name} `);
+
+        }
+
+        console.log(jsUser.greeting);
+        console.log(jsUser.greetingTow);
+
