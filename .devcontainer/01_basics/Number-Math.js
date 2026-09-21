8178b0dc16e569e43dcc4c3578ec93b3e6988f37
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-21 05:21:21 +0000

    js

---
+++ .devcontainer/01_basics/Number-Math.js
@@ -0,0 +1,19 @@
+//   +++++++++++++++++++++++ Maths section +++++++++++++++++++++++++++\\
+
+
+
+        console.log(Math);
+        console.log(Math.abs(+4));
+        console.log(Math.round(4.3));
+        console.log(Math.ceil(4.6));
+        console.log(Math.floor(4.6));
+
+
+        console.log(Math.random());
+        console.log((Math.random() * 10) + 1);
+        console.log((Math.random() * 10) + 1);
+
+        let min = 10
+        let max = 20
+
+        console.log(Math.floor(Math.random() * (max - min + 1)))
