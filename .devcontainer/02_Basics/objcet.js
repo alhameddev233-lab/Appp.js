9be64b068d5f0cc61c0ab2ba5603245799dfc781
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-21 10:11:39 +0000

    .

--- .devcontainer/02_Basics/objcet.js
+++ .devcontainer/02_Basics/objcet.js
@@ -1,39 +1,39 @@
 
+// Object-1
+// Single-Ton
 
-        // Single-Ton
+// Object-Literals 
 
-        // Object-Literals 
+const mySym = Symbol("Key1")
+let jsUser = {
+    name: "Ahmed",
+    "fullname": "Ahmed AL-Hamed",
+    [mySym]: "Keysym-1",
+    age: 21,
+    location: "Pune",
+    email: "alhameda224@gmail.com",
+    isLogin: false,
+}
+// console.log(jsUser.name);
+// console.log(jsUser["location"]);
+// console.log(jsUser["fullname"]);
+// console.log(jsUser[mySym]);
 
-        const mySym = Symbol("Key1")
-        let jsUser = {
-            name: "Ahmed",
-            "fullname": "Ahmed AL-Hamed",
-            [mySym]: "Keysym-1",
-            age: 21,
-            location: "Pune",
-            email: "alhameda224@gmail.com",
-            isLogin: false,
-        }
-        // console.log(jsUser.name);
-        // console.log(jsUser["location"]);
-        // console.log(jsUser["fullname"]);
-        // console.log(jsUser[mySym]);
+jsUser.email = "AlhmedDev@gmail.com"
+// Object.freeze(jsUser)
 
-        jsUser.email = "AlhmedDev@gmail.com"
-        // Object.freeze(jsUser)
+jsUser.email = "AlhmedDev@google.com"
+// console.log(jsUser);
 
-        jsUser.email = "AlhmedDev@google.com"
-        // console.log(jsUser);
+jsUser.greeting = function () {
+    console.log("Hello js user");
 
-        jsUser.greeting = function () {
-            console.log("Hello js user");
+}
+jsUser.greetingTow = function () {
+    console.log(`Hello js user, ${this.name} `);
 
-        }
-        jsUser.greetingTow = function () {
-            console.log(`Hello js user, ${this.name} `);
+}
 
-        }
-
-        console.log(jsUser.greeting);
-        console.log(jsUser.greetingTow);
+console.log(jsUser.greeting);
+console.log(jsUser.greetingTow);
 
