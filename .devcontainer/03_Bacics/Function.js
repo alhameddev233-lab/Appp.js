96c0a2f8d69931cb4c4abf20f88bb1ac74306b97
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-21 11:09:25 +0000

    Function

---
+++ .devcontainer/03_Bacics/Function.js
@@ -0,0 +1,13 @@
+  // function abc() {
+        //     console.log("Ahmed");
+
+        // }
+
+        // abc()
+
+        function add(Number1, Number2) {
+            let result = Number1 + Number2
+            return result
+        }
+        let result = add(5, 5)
+        console.log("Reslut :", result);
