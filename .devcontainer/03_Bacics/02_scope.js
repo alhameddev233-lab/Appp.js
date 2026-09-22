1cbc3453dc24e96af191ed55b1f4166b3372038a
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-22 10:32:07 +0000

    interesting

---
+++ .devcontainer/03_Bacics/02_scope.js
@@ -0,0 +1,43 @@
+
+  function one() {
+            const username = "Ahmed"
+
+            function tow() {
+                const website = "youtube"
+                console.log(username);
+
+            }
+            // console.log(website);
+            tow()
+
+        }
+
+        one()
+
+        if (true) {
+            const userName = "Ahmed"
+            if (userName === "Ahmed") {
+                const webSite = "Youtube"
+                console.log(userName + webSite);
+
+            }
+            console.log(webSite);
+
+        }
+        console.log(userName);
+
+
+        // ++++++++++++++++++++ interesting ++++++++++++++++++++++++ \\
+
+
+
+        function addone(num) {
+            return num + 1
+        }
+        addone(5)
+        const addtow = function (num) {
+            return num + 2
+        }
+
+        addtow(5)
+
