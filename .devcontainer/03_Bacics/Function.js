af76664f3952520165b20cc1da090648f4022fa0
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-22 06:57:37 +0000

    function

--- .devcontainer/03_Bacics/Function.js
+++ .devcontainer/03_Bacics/Function.js
@@ -11,3 +11,33 @@
         }
         let result = add(5, 5)
         console.log("Reslut :", result);
+
+        const user = {
+            itemName: "Appple",
+            price: 400,
+        }
+
+
+        function handleobject(anyitems) {
+            console.log(`itesmName is ${anyitems.itemName} and price is ${anyitems.price}`);
+
+
+        }
+
+
+        // handleobject(user)
+        handleobject({
+            itemName: "Mango",
+            price: 200
+        })
+
+        let array = [200, 300, 400, 100,]
+
+        function returnSeconValue(getArry) {
+            return getArry[1]
+
+
+        }
+        // console.log(returnSeconValue(array));
+        console.log(returnSeconValue([200, 400, 500, 1000]));
+
