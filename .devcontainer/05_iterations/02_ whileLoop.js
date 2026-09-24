801985959dc770b3366b0538d48dec07b78b9233
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-24 04:44:13 +0000

    while loop do while loop

---
+++ .devcontainer/05_iterations/02_ whileLoop.js
@@ -0,0 +1,17 @@
+ //while loop do while loop
+
+
+        let index = 0
+        while (index <= 10) {
+            console.log(`Value Of Index is ${index}`);
+            index = index + 2
+
+        }
+
+        let Hero = ["Iron Man", "Hulk", "Thor"]
+        let i = 0
+
+        while (i < Hero.length) {
+            console.log(`Value is ${Hero[i]}`);
+            i = i + 1
+        }
