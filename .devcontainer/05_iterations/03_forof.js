b4290091e0cef3683e2ec304df3bbdec38c0d4e6
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-24 07:39:38 +0000

    For Of

---
+++ .devcontainer/05_iterations/03_forof.js
@@ -0,0 +1,31 @@
+ //  For Of 
+
+        let i = [
+            {
+                name: "Ahmed",
+                age: 21,
+                City: "Goa"
+            },
+            {
+                name: "Ahmed",
+                age: 31,
+                City: "Goa"
+            },
+        ]
+
+        for (const num of i) {
+            console.log(num.age);
+
+        }
+
+        let greetings = "HELLO WORLD !"
+
+        for (const greet of greetings) {
+            console.log(`Each Chart is ${greet}`);
+
+
+        }
+
+
+        // Maps 
+
