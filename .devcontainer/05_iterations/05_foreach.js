8007ed7b86200d9f37bcc07f09ddd7b00f682ea1
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-24 09:56:31 +0000

    forEach

---
+++ .devcontainer/05_iterations/05_foreach.js
@@ -0,0 +1,45 @@
+// ForEcah
+
+
+// let coding = ['js', 'ruby', 'python', 'java', 'cpp', ' Swift']
+
+
+// coding.forEach(function (item) {
+//     console.log(item);
+
+// })
+
+// function printMe(item) {
+//     console.log();
+
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+
+// })
+
+const coding = [
+    {
+        name: "Ahmed",
+        age: 21,
+        City: "Goa"
+    },
+    {
+        name: "Umer",
+        age: 22,
+        City: "Pune"
+    },
+    {
+        name: "Xyz",
+        age: 1,
+        City: "abs"
+    },
+]
+
+coding.forEach((code) => {
+    console.log(code.name);
+
+})
