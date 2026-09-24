747a333c68f577ff469b8523d7e299983e420883
alhameddev233-lab <alhameddev233@gmail.com>
2026-09-24 07:39:47 +0000

    maps

---
+++ .devcontainer/05_iterations/04_Maps .js
@@ -0,0 +1,26 @@
+
+        Maps 
+
+        let Maps = new Map()
+        map.set('Ind', 'India')
+        map.set('Usa', 'Unite States of America')
+        map.set('Fr', 'France')
+        console.log(map);
+
+        for (const [key, value] of map) {
+            console.log(key, ':=>', value);
+
+        }
+
+     
+
+
+        for (const [key, value] of myObject) {
+            console.log(key, ':=>', value);
+
+        }
+
+        let map = new Map()
+        map.set('Ind', 'India')
+        map.set('Usa', 'Unite States of America')
+        map.set('Fr', 'France')
